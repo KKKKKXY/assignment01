a571e517b159ebba1fec09eb7bafb3778fbb52fd
JunfengGuan <lucienguan@gmail.com>
2019-02-02 23:39:11 +0800

    Create character2 Score feature

--- src/main/java/model/Score2.java
+++ src/main/java/model/Score2.java
@@ -1,4 +1,22 @@
 package model;
 
-public class Score2 {
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+public class Score2 extends Pane {
+
+    Label point;
+
+    public Score2(int x, int y) {
+        point = new Label("0");
+        setTranslateX(x);
+        setTranslateY(y);
+
+    }
+    public void setPoint(int score) {
+        this.point.setText(Integer.toString(score));
+    }
 }
