e8a377f689c2e3be2173837d92742ed32a245f05
xingyuan kang <myaploy@gmail.com>
2019-02-02 21:45:04 +0700

    Set size and style for bar And add it to scene

--- src/main/java/model/LifeBar.java
+++ src/main/java/model/LifeBar.java
@@ -1,4 +1,29 @@
 package model;
 
-public class LifeBar {
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+import javax.swing.*;
+
+public class LifeBar extends Pane {
+    ProgressBar pbar;
+    public LifeBar(int x, int y) {
+        setTranslateX(x);
+        setTranslateY(y);
+        pbar = new ProgressBar();
+        pbar.setScaleX(5);
+        pbar.setProgress(100);
+        pbar.setMaxSize(100,30);
+        pbar.setStyle("-fx-background-color: -fx-box-border");
+        pbar.setTranslateX(100);
+        getChildren().add(pbar);
+    }
+
+    public void setLifeBar(double lifePercentage){
+        this.pbar.setProgress(lifePercentage/50);
+    }
 }
